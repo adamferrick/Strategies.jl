9feb229dd069d46ee6212ee89f93aeb780de3601
Adam Ferrick <adamferrick@example.com>
2024-08-06 18:32:14 -0700

    do not set the beginning of the series to s0

--- src/simulation.jl
+++ src/simulation.jl
@@ -6,12 +6,13 @@ end
 
 function correlated_gbm_trajectories(s0::Vector{Float64}, r::Float64, volatilities::Vector{Float64}, epsilon::Matrix{Float64}, dt::Float64, n::Int64)
     L = cholesky(epsilon).L
-    trajectories = [fill(s0[i], n) for i in 1:length(s0)]
+    trajectories = [zeros(n) for i in 1:length(s0)]
     X = rand(Normal(), (length(s0), n))
     Y = L * X
     for i in 1:length(s0)
-        for j in 2:n
-            trajectories[i][j] = trajectories[i][j-1] * exp((r - 0.5 * volatilities[i]^2) * dt + volatilities[i] * sqrt(dt) * Y[i, j])
+        for j in 1:n
+            prev = j == 1 ? s0[i] : trajectories[i][j-1]
+            trajectories[i][j] = prev * exp((r - 0.5 * volatilities[i]^2) * dt + volatilities[i] * sqrt(dt) * Y[i, j])
         end
     end
     trajectories
